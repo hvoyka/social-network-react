1a76a95be524035cc8a4828bb56e8f57083aece0
Lavrentev Stanislav <hvoinii@gmail.com>
2020-05-11 13:21:15 +0600

    add html to Dialogs components and minimal css

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -3,7 +3,21 @@ import s from './Dialogs.module.css';
 
 const Dialogs = (props) => {
   return (
-    <div>Dialogs</div>
+    <div className={s.dialogs}>
+      <div className={s.items}>
+        <div className={s.item + ' ' + s.active}>Dimych</div>
+        <div className={s.item}>Andrey</div>
+        <div className={s.item}>Sveta</div>
+        <div className={s.item}>Sasha</div>
+        <div className={s.item}>Victor</div>
+        <div className={s.item}>Valera</div>
+      </div>
+      <div className={s.messages}>
+        <div className={s.message}>Hello</div>
+        <div className={s.message}>How is your work?</div>
+        <div className={s.message}>Yo!</div>
+      </div>
+    </div>
   );
 }
 export default Dialogs;
