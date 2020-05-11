22df5055b29cf986b9a975363f3b8f32e7fc612a
Lavrentev Stanislav <hvoinii@gmail.com>
2020-05-11 11:56:32 +0600

    install react-router-dom, add routes and new components News, Music, Settings

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -4,11 +4,21 @@ import  s from './Navbar.module.css';
 const Navbar = () => {
   return <nav className={s.nav}>
             <ul className={s.list}>
-              <li className={`${s.item} ${s.active}`}><a href='#'>Profile</a></li>
-              <li className={s.item}><a href='#'>Messages</a></li>
-              <li className={s.item}><a href='#'>News</a></li>
-              <li className={s.item}><a href='#'>Music</a></li>
-              <li className={s.item}><a href='#'>Settings</a></li>
+              <li className={`${s.item} ${s.active}`}>
+                <a href='/profile'>Profile</a>
+              </li>
+              <li className={s.item}>
+                <a href='/dialogs'>Messages</a>
+              </li>
+              <li className={s.item}>
+                <a href='/news'>News</a>
+              </li>
+              <li className={s.item}>
+                <a href='/music'>Music</a>
+              </li>
+              <li className={s.item}>
+                <a href='/settings'>Settings</a>
+              </li>
             </ul>
           </nav>;
 }
