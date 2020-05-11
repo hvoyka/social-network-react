7ee0697cc3158f09a1a53c45810409c1f493f015
Lavrentev Stanislav <hvoinii@gmail.com>
2020-05-11 11:09:27 +0600

    create component Dialog nad move css for contents to app.css

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -4,7 +4,7 @@ import MyPosts from './MyPosts/MyPosts';
 
 const Profile = () => {
   return (
-    <div className={s.content}>
+    <div>
       <div className='content__img-box'>
         <img className='content__img' src='https://www.w3schools.com/howto/img_snow_wide.jpg' />
       </div>
