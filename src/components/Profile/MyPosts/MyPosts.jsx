73d14626915bd515a198f95610dfd0d74df78c7a
Lavrentev Stanislav <hvoinii@gmail.com>
2020-05-13 17:34:05 +0600

    take value from textarea with React.createRef()

--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -5,15 +5,18 @@ import Post from './Post/Post';
 const MyPosts = (props) => {
 
   let postComponentsArr = props.posts.map((p) => <Post message={p.message} likesCount={p.likesCount}  key={p.id}/>);
+  let newPostElement = React.createRef();
+
   const addPost = () => {
-    alert('hehey');
+    let text = newPostElement.current.value;
+    alert(text);
   }
   return (
     <div className={css.wrap}>
      <h3>My Posts</h3> 
       <div>
         <div>
-          <textarea></textarea>
+          <textarea ref={newPostElement}></textarea>
         </div>
         <button onClick={addPost}>Add post</button>
       </div>
