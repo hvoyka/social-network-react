fa135ba771a7c6cfa1f5c37398db75b167ea4df3
Lavrentev Stanislav <hvoinii@gmail.com>
2020-06-09 18:12:16 +0600

    create function getusers and add button 'add users'

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -3,34 +3,37 @@ import css from './users.module.css'
 import * as axios from 'axios';
 import userPhoto from "../../assets/images/user.png"
 const Users = (props) => {
-  if(props.users.length === 0){
 
-    axios.get("https://social-network.samuraijs.com/api/1.0/users?count=20")
-          .then(response => {
-
-            props.setUsers(response.data.items);
-          });
-
-    
-  }
- 
-  return (
-    <ul>{
-      props.users.map(u => <li key={u.id}>
-        <div>
-          <div><img src={u.photos.small !== null ? u.photos.small : userPhoto} alt="" width="50px" /></div>
-          {
-            u.followed
-              ? <button onClick={() => props.unfollow(u.id)}>Unfollow</button>
-              : <button onClick={() => props.follow(u.id)}>Follow</button>}
-        </div>
-        <div>
-          <div>{u.name}</div>
-          <div>{u.status}</div>
-        </div>
-      </li>)
+  const getUsers = () => {
+    if (props.users.length === 0) {
+      axios.get("https://social-network.samuraijs.com/api/1.0/users")
+        .then(response => {
+          props.setUsers(response.data.items);
+        });
     }
-    </ul>
+  }
+
+
+  return (
+    <div>
+      <button onClick={getUsers}>Get Users</button>
+      <ul>{
+        props.users.map(u => <li key={u.id}>
+          <div>
+            <div><img src={u.photos.small !== null ? u.photos.small : userPhoto} alt="" width="50px" /></div>
+            {
+              u.followed
+                ? <button onClick={() => props.unfollow(u.id)}>Unfollow</button>
+                : <button onClick={() => props.follow(u.id)}>Follow</button>}
+          </div>
+          <div>
+            <div>{u.name}</div>
+            <div>{u.status}</div>
+          </div>
+        </li>)
+      }
+      </ul>
+    </div>
   );
 }
 export default Users;
