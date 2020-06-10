ef6d64f1c7659086f89c5145b83dfc31233a33a5
Lavrentev Stanislav <hvoinii@gmail.com>
2020-06-10 16:49:52 +0600

    add componentdidmount, and remove constructor

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -5,17 +5,15 @@ import userPhoto from "../../assets/images/user.png"
 
 
 class Users extends React.Component {
-  constructor (props) {
-    super(props);
-
+  
+  componentDidMount() {
     if (this.props.users.length === 0) {
       axios.get("https://social-network.samuraijs.com/api/1.0/users")
         .then(response => {
           this.props.setUsers(response.data.items);
         });
-    }    
+    }   
   }
- 
   render() {
     return (
       <div>
