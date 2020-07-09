d20a38e297015b39c1011ab2a2c1667c0f04633a
Lavrentev Stanislav <hvoinii@gmail.com>
2020-07-09 09:44:12 +0600

    add withRouter to ProfileContainer, and add userId parametr to /profile in App.js

--- src/components/Profile/ProfileContainer.jsx
+++ src/components/Profile/ProfileContainer.jsx
@@ -3,10 +3,12 @@ import Profile from './Profile';
 import * as axios from 'axios';
 import { connect } from 'react-redux';
 import { setUserProfile } from '../../redux/profile-reducer';
+import { withRouter } from 'react-router-dom';
 
 class ProfileContainer extends React.Component {
 
   componentDidMount() {
+
     axios.get(`https://social-network.samuraijs.com/api/1.0/profile/2`)
     .then(response => {
       this.props.setUserProfile(response.data);
@@ -28,4 +30,4 @@ const mapStateToProps = (state) => {
 
 export default connect(mapStateToProps,{
   setUserProfile
-})(ProfileContainer);
+})(withRouter(ProfileContainer));
