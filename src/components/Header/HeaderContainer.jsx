9af614762ef26af620bfa391d51eca970b9f1dbd
Lavrentev Stanislav <hvoinii@gmail.com>
2020-07-13 17:42:33 +0600

    In HeaderComponent add request for auth, with cookies

--- src/components/Header/HeaderContainer.jsx
+++ src/components/Header/HeaderContainer.jsx
@@ -1,8 +1,16 @@
 import React from 'react';
 import Header from './Header';
+import * as axios from 'axios';
 
-
-class HeaderContainer extends React.Component() {
+class HeaderContainer extends React.Component {
+  componentDidMount () {
+    axios.get(`https://social-network.samuraijs.com/api/1.0/auth/me`, {
+      withCredentials: true
+    })
+    .then(response => {
+      debugger
+    });
+  }
   render() {
     return <Header {...this.props}/>;
   }
