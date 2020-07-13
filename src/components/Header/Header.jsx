554df5eb5c7af3da45d97f8f5aa3174cf669d26b
Lavrentev Stanislav <hvoinii@gmail.com>
2020-07-13 17:24:59 +0600

    In Header add NavLink to /login page

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
 import css from './Header.module.css';
+import { NavLink } from 'react-router-dom';
 
 const Header = () => {
   return <header className={css.header}>
-  <img className={css.logo} src="https://avatars3.githubusercontent.com/u/46483437?s=460&v=4"/>
-</header>;
+    <img className={css.logo} src="https://avatars3.githubusercontent.com/u/46483437?s=460&v=4" />
+    <div className={css.loginBlock}>
+      <NavLink to={'/login'}>Login</NavLink>
+    </div>
+  </header>;
 }
 export default Header;
