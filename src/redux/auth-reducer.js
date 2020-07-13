fd9603734ba3791b98c7885d3f75bda13fe1277b
Lavrentev Stanislav <hvoinii@gmail.com>
2020-07-13 17:05:39 +0600

    create auth-reducer

---
+++ src/redux/auth-reducer.js
@@ -0,0 +1,27 @@
+const SET_USER_DATA = 'SET_USER_DATA';
+const UNFOLLOW = 'UNFOLLOW';
+
+
+let initialState = {
+  userId: null,
+  email: null,
+  login: null
+};
+
+export const authReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case SET_USER_DATA:
+      return {
+        ...state,
+        ...action.data
+      }
+    
+    default:
+      return state;
+  }
+
+}
+
+export const setUserData = (userId, email, login) => ({ type: SET_USER_DATA, data: {userId, email, login} });
+
+export default authReducer;
