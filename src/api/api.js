7a25e560e9c98eb0d0c2c5d914f11f65eb8c8d1b
Lavrentev Stanislav <hvoinii@gmail.com>
2020-07-15 16:55:31 +0600

    create instance of axios.create functions with default params

--- src/api/api.js
+++ src/api/api.js
@@ -1,7 +1,13 @@
 import * as axios from 'axios';
 
+const instance = axios.create({
+  withCredentials: true,
+  headers: {
+    'API-KEY': 'e581219b-e76c-405e-b89a-4033e27a3e38'
+  },
+  baseURL: 'https://social-network.samuraijs.com/api/1.0/',
+});
+
 export const getUsers = (currentPage = 1, pageSize = 10) => {
-  return axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${currentPage}&count=${pageSize}`, {
-    withCredentials: true
-  }).then(response => response.data);
+  return instance.get(`users?page=${currentPage}&count=${pageSize}`).then(response => response.data);
 }
