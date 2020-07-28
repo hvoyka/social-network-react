345099925a326c47177a612b35166728d40c31b3
Lavrentev Stanislav <hvoinii@gmail.com>
2020-07-28 11:38:58 +0600

    change logo in header, add svg on background for app-wrapper

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import css from './Header.module.css';
 import { NavLink } from 'react-router-dom';
+import Logo from '../../assets/images/forest.png';
+
 
 
 
 const Header = (props) => {
   return <header className={css.header}>
-    <img className={css.logo} src="https://avatars3.githubusercontent.com/u/46483437?s=460&v=4" alt="avatar"/>
+
+    <img className={css.logo} src={Logo} alt="logo-social-network"/>
     <div className={css.loginBlock}>
       {props.isAuth ? props.login :
       <NavLink to={'/login'}>Login</NavLink>}
