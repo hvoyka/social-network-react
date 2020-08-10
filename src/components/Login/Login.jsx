a5c3935ec42a9a2a0ed1ae5fca8f2dc1abe5be2e
Lavrentev Stanislav <hvoinii@gmail.com>
2020-08-10 13:28:47 +0600

    add html form to Login component

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,8 +1,33 @@
 import React from 'react';
 
+const LoginForm = (props) => {
+  return (
+    <form>
+      <div>
+        <input placeholder={"Login"} />
+      </div>
+      <div>
+        <input placeholder={"Password"} />
+      </div>
+      <div>
+        <label>
+          <input type={"checkbox"} />
+            Remember Me
+          </label>
+      </div>
+      <div>
+        <button>Login</button>
+      </div>
+    </form>
+  );
+}
+
 const Login = (props) => {
   return (
-    <h1>LOGIN</h1>
+    <div>
+      <h1>LOGIN</h1>
+      <LoginForm />
+    </div>
   );
 }
 export default Login;
