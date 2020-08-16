74c861c3786201d9e874d3301b001c2a0823429b
Lavrentev Stanislav <hvoinii@gmail.com>
2020-08-16 17:11:08 +0600

    add validation to textarea in formscontrols

--- src/components/common/Preloader/FormsControls/FormsControls.js
+++ src/components/common/Preloader/FormsControls/FormsControls.js
@@ -1,9 +1,12 @@
 import React from "react";
+import css from './FormsControls.module.css';
 
 export const Textarea = ({input, meta, ...props}) => {
+  const hasError = meta.touched && meta.error;
   return (
-    <div>
+    <div className={css.formControl + " " + (hasError ? css.error : "")} >
       <textarea {...props} {...input}/>
+      {hasError && <p>{meta.error}</p>}      
     </div>
   );
 }
