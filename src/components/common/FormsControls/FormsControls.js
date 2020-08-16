28be4f68dc59e83c423933277506de2fa31a6e23
Lavrentev Stanislav <hvoinii@gmail.com>
2020-08-16 19:23:30 +0600

    more refactoring for formcontrols, create new element with react.crateelement, and in props throw type of filds

--- src/components/common/FormsControls/FormsControls.js
+++ src/components/common/FormsControls/FormsControls.js
@@ -1,29 +1,23 @@
 import React from "react";
 import css from './FormsControls.module.css';
 
-const FormControl = ({input, meta, child, ...props}) => {
+
+const FormControl = ({ input, meta, fildType, ...props }) => {
   const hasError = meta.touched && meta.error;
   return (
     <div className={css.formControl + " " + (hasError ? css.error : "")} >
-      {props.children}
-      {hasError && <p>{meta.error}</p>}      
+      {React.createElement(fildType, {...input, ...props})} 
+      {hasError && <p>{meta.error}</p>}
     </div>
   );
 }
-
 export const Textarea = (props) => {
-  const {input, meta, child, ...restProps} = props;
   return (
-    <FormControl {...props}>
-      <textarea {...restProps} {...input}/>
-    </FormControl>
- );
+    <FormControl {...props} fildType="textarea"/>
+  );
 }
 export const Input = (props) => {
-  const {input, meta, child, ...restProps} = props;
   return (
-      <FormControl {...props}>
-       <input {...restProps} {...input}/>
-      </FormControl>
+    <FormControl {...props} fildType="input"/>
   );
 }
